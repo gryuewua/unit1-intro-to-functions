807ba3446ae6c50602293e300efc7c3301ef4f7c
gryuewua <yuchenw7@nycstudents.net>
2026-01-28 09:23:55 -0500

    day 1
    a

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+for x in range(60):
+    t.right(5)
+    for y in range(4):
+        t.forward(100)
+        t.left(90)
+
+
+turtle.done()
